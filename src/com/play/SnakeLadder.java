990a78233fa352d142e78dc518e6befdbc49a450
avnish986 <avnishgupta986@gmail.com>
2020-09-24 17:22:49 +0530

    [Avnish]Added:UC1

--- src/com/play/SnakeLadder.java
+++ src/com/play/SnakeLadder.java
@@ -1,9 +1,12 @@
 package com.play;
 
 public class SnakeLadder {
-
+	public static final int player_count=1;
 	public static void main(String[] args) {
 		System.out.println("Welcome to Snake and Ladder game");
+		
+		int position = 0;
+		System.out.println("Player at : "+position);
 
 	}
 
