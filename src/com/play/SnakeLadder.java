879755a18db46ecb66f1dff0248c5e7a68b9c7f6
avnish986 <avnishgupta986@gmail.com>
2020-09-24 17:26:17 +0530

    [Avnish]Added:UC3

--- src/com/play/SnakeLadder.java
+++ src/com/play/SnakeLadder.java
@@ -2,6 +2,9 @@ package com.play;
 
 public class SnakeLadder {
 	public static final int player_count=1;
+	public static final int noPlay=0;
+	public static final int ladder=1;
+	public static final int snake=2;
 	public static void main(String[] args) {
 		System.out.println("Welcome to Snake and Ladder game");
 		
@@ -10,6 +13,19 @@ public class SnakeLadder {
 		
 		int valDice=(int)(Math.floor(Math.random()*10)%6)+1;
 		System.out.println("player rolled no. "+valDice);
+		
+		int option=(int)(Math.floor(Math.random()*10)%3);
+		
+		if(option == noPlay)
+			System.out.println("Player stays at "+position);
+		else if(option == ladder) {
+			position+=valDice;
+			System.out.println("Player moves ahead at "+position);
+		}
+		else {
+			position-=valDice;
+			System.out.println("Player moves back at "+position);
+		}
 
 	}
 
